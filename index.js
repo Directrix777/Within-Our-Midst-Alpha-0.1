7c72f10b68fd6e93147ecd925d593bc612015652
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-21 11:33:58 -0500

    Created js files

---
+++ index.js
@@ -0,0 +1,8 @@
+let chat = document.createElement('div')
+chat.id= "chat"
+let hide = document.createElement('input')
+hide.type = 'submit'
+hide.value = 'Hide Chat'
+hide.addEventListener('click', function() {chat.className = 'hidden'})
+document.body.appendChild(hide)
+document.body.appendChild(chat)
