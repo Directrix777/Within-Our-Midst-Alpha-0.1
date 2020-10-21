7ad4cb722ef132e5df87db6e5963c47bf005c806
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-21 14:01:26 -0500

    Added backend

--- frontend/index.js
+++ frontend/index.js
@@ -6,3 +6,7 @@ hide.value = 'Hide Chat'
 hide.addEventListener('click', function() {chat.className = 'hidden'})
 document.body.appendChild(hide)
 document.body.appendChild(chat)
+
+fetch('http://localhost:3000/messages')
+.then(response => response.json())
+.then(console.log(response))
